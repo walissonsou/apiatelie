0e87614603a451f764a14a66524213e5c29d6d97
walissonsou <walissonsouza96@hotmail.com>
2022-06-13 19:14:11 -0300

    add cors

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const port = process.env.PORT || 3000;
 const produtos = require("./src/products/produtos.json")
 
+var cors = require('cors')
+
+
+app.use(cors())
+
 app.get('/products', (req, res) => {
   return res.json(produtos)
 })
